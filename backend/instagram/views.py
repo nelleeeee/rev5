54aae5c979f347b28a85e371c33d1aa0fac2c8c8
nelleeeee <rudghksldl@gmail.com>
2020-08-24 00:45:24 +0900

    포슨스팅 목록에 인증적용 및 필터 적용

--- backend/instagram/views.py
+++ backend/instagram/views.py
@@ -3,9 +3,22 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 from .models import Post
 from .serializers import PostSerializer
+from datetime import timedelta
+from django.db.models import Q
+from django.utils import timezone
 
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [AllowAny]  # FIXME: 인증적용해야함
+    # permission_classes = [AllowAny]  # FIXME: 인증적용해야함
+
+    def get_queryset(self):
+        timesince = timezone.now() - timedelta(days=3)
+        qs = super().get_queryset()
+        qs = qs.filter(
+            Q(author=self.request.user)
+            | Q(author__in=self.request.user.following_set.all())
+        )
+        qs = qs.filter(created_at__gte=timesince)
+        return qs
