0cf41031dd2fd792245cfdd04b1263aebfbc1feb
nelleeeee <rudghksldl@gmail.com>
2020-08-26 02:26:43 +0900

    좋아요 구현

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useAxios from "axios-hooks";
 import Post from "./Post";
 import { useAppContext } from "store";
 import { Alert } from "antd";
+import Axios from "axios";
 
 // const apiUrl = "http://localhost:8000/api/post/";
 
@@ -11,6 +12,8 @@ function PostList() {
     store: { jwtToken },
     // dispatch,
   } = useAppContext();
+
+  const [postList, setPostList] = useState([]);
 
   // const [postList, setPostList] = useState([]);
 
@@ -30,17 +33,48 @@ function PostList() {
 
   const headers = { Authorization: `JWT ${jwtToken}` };
 
-  const [{ data: postList, loading, error }, refetch] = useAxios({
+  const [{ data: originPostList, loading, error }, refetch] = useAxios({
     url: "http://localhost:8000/api/post/",
     headers,
   });
+
+  useEffect(() => {
+    setPostList(originPostList);
+  }, [originPostList]);
+
+  const handleLike = async ({ post, isLike }) => {
+    const apiUrl = `http://localhost:8000/api/post/${post.id}/like/`;
+    const method = isLike ? "POST" : "DELETE";
+
+    try {
+      const response = await Axios({
+        url: apiUrl,
+        method,
+        headers,
+      });
+      console.log("response :", response);
+
+      setPostList((prevList) => {
+        return prevList.map((currentPost) =>
+          currentPost === post
+            ? { ...currentPost, is_like: isLike }
+            : currentPost
+        );
+      });
+    } catch (error) {
+      console.log("error :", error);
+    }
+  };
 
   return (
     <div>
       {postList && postList.length === 0 && (
         <Alert type="warning" message="포스팅이 없습니다" />
       )}
-      {postList && postList.map((post) => <Post post={post} key={post.id} />)}
+      {postList &&
+        postList.map((post) => (
+          <Post post={post} key={post.id} handleLike={handleLike} />
+        ))}
     </div>
   );
 }
